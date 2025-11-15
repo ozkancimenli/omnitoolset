941df0fab905811b34391cb4f1302e059a69dc86
Ozkan Cimenli <ozkan.cimenli@hotmail.com>
2025-11-14 22:24:30 -0500

    Fix Adsterra: use original code exactly - script per container with original container ID

--- components/Adsterra.tsx
+++ components/Adsterra.tsx
@@ -1,5 +1,8 @@
 'use client';
 
+import { useEffect } from 'react';
+import Script from 'next/script';
+
 interface AdsterraProps {
   containerId?: string;
   layout?: '1x1' | '1x2' | '2x1' | '4x1' | 'auto';
@@ -13,8 +16,8 @@ export default function Adsterra({
   className = '',
   style,
 }: AdsterraProps) {
-  // Use original container ID - Adsterra script looks for this specific ID
-  // For multiple instances, we'll use the same ID (Adsterra handles multiple containers)
+  // Use original container ID exactly as Adsterra provided
+  // Adsterra script will inject ads into this container
 
   // Layout styles
   const layoutStyles: Record<string, React.CSSProperties> = {
@@ -26,14 +29,25 @@ export default function Adsterra({
   };
 
   return (
-    <div
-      id={containerId}
-      className={`adsterra-container ${className}`}
-      style={{
-        ...layoutStyles[layout],
-        ...style,
-        margin: '0 auto',
-      }}
-    />
+    <>
+      {/* Adsterra Script - Original code from Adsterra */}
+      <Script
+        async
+        data-cfasync="false"
+        src="//pl28055637.effectivegatecpm.com/612a325632297ecc15cfd2d178f355ec/invoke.js"
+        strategy="afterInteractive"
+      />
+      
+      {/* Adsterra Container - Original code from Adsterra */}
+      <div
+        id={containerId}
+        className={`adsterra-container ${className}`}
+        style={{
+          ...layoutStyles[layout],
+          ...style,
+          margin: '0 auto',
+        }}
+      />
+    </>
   );
 }
