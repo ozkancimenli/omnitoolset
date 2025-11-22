ee0413c73583883a0ec0eb612bf8ae963e0d7a9c
Ozkan Cimenli <ozkan.cimenli@hotmail.com>
2025-11-22 12:17:20 -0500

    fix: Update hooks index to export all new hooks

--- components/tools/pdf-editor/hooks/index.ts
+++ components/tools/pdf-editor/hooks/index.ts
@@ -1,5 +1,8 @@
 // Export all hooks
 export { usePdfLoader } from './usePdfLoader';
 export { useTextEditing } from './useTextEditing';
+export { useZoom } from './useZoom';
+export { useCanvas } from './useCanvas';
+export { useAnnotations } from './useAnnotations';
 
 
